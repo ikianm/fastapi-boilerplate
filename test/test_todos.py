2faad41fd4860d21cefe0461158ccb9cbdd535a3
ikianm <ikianm2002@gmail.com>
2025-03-23 14:12:33 +0330

    test(unitTest): set up testing environment & test some routes

---
+++ test/test_todos.py
@@ -0,0 +1,89 @@
+import pytest
+from fastapi.testclient import TestClient
+from fastapi import status
+from sqlalchemy import create_engine, text
+from sqlalchemy.pool import StaticPool
+from sqlalchemy.orm import sessionmaker
+from ..database import Base
+from ..main import app
+from ..routers.todos import get_db, get_current_user
+from ..models import Todos
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./testdb.db'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}, poolclass=StaticPool)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+def override_get_current_user():
+    return {'username': 'ikianm', 'id': 1, 'user_role': 'admin'}
+        
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+client = TestClient(app)
+
+
+
+@pytest.fixture
+def test_todo():
+    todo = Todos(
+        title="learn to code",
+        description="need to learn everyday",
+        priority=5,
+        complete=False,
+        owner_id=1 # same id as user dependency
+    )
+    
+    db = TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    yield todo
+    with engine.connect() as connection:
+        connection.execute(text('DELETE FROM todos;'))
+        connection.commit()
+
+def test_read_all_authenticated(test_todo):
+    response = client.get('/')
+    assert response.status_code ==  status.HTTP_200_OK
+    assert response.json() == [
+            {
+                "id": 1,
+                "title": "learn to code", 
+                "description": "need to learn everyday", 
+                "priority": 5,
+                "complete": False,
+                "owner_id": 1
+            }
+        ]
+    
+    
+def test_read_one_authenticated(test_todo):
+    response = client.get('/todo/1')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+                "id": 1,
+                "title": "learn to code", 
+                "description": "need to learn everyday", 
+                "priority": 5,
+                "complete": False,
+                "owner_id": 1
+            }
+        
+
+def test_read_one_authenticated_not_found(test_todo):
+    response = client.get('/todo/2')
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {"detail": "todo not found"}
+    
+    
